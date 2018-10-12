78cc0df5dd90ac6582237622fd8816e9a1cecd9e
Mofe Ejegi <mofe.ejegi@apmis.ng>
2018-10-12 17:07:28 +0100

    Sending and receiving via sockets and REST

--- app/src/main/java/ng/apmis/audreymumplus/ui/Chat/chatforum/ChatForumFragment.java
+++ app/src/main/java/ng/apmis/audreymumplus/ui/Chat/chatforum/ChatForumFragment.java
@@ -1,7 +1,5 @@
 package ng.apmis.audreymumplus.ui.Chat.chatforum;
 
-import android.arch.lifecycle.ViewModel;
-import android.arch.lifecycle.ViewModelProvider;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.content.Intent;
@@ -9,7 +7,6 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.util.DiffUtil;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -21,22 +18,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.TextView;
-
-import com.github.nkzawa.socketio.client.Socket;
-import com.google.gson.Gson;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import ng.apmis.audreymumplus.AudreyMumplus;
 import ng.apmis.audreymumplus.R;
 import ng.apmis.audreymumplus.data.network.ChatSocketService;
 import ng.apmis.audreymumplus.ui.Chat.ChatContextFragment;
